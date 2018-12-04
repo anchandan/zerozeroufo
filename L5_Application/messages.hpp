25118d45c8dad2249f3da3d5cb371dfd25ae95aa
Oliver Zhu <ollie.zhu@gmail.com>
2018-12-04 11:11:24 -0800

    common wireless messaging code

---
+++ L5_Application/messages.hpp
@@ -0,0 +1,20 @@
+/*
+ * messages.hpp
+ *
+ *  Created on: Dec 4, 2018
+ *      Author: ollie
+ */
+
+#ifndef MESSAGES_HPP_
+#define MESSAGES_HPP_
+
+
+#include "wireless.h"
+
+
+/* Message opcodes */
+const uint32_t ZZU_DATA = 0;
+const uint32_t ZZU_CTRL = 1;
+
+
+#endif /* MESSAGES_HPP_ */
